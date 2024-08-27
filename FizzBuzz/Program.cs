8989af8f7ea4ed20556cf11de51f5ccbca543b46
Erik Bäckström <Erik Bckstrm@DESKTOP-MEVT6FK>
2024-08-27 12:16:26 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number - 1 < 100; number++)
+            {
+                if (number % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
